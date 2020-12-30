f0e2cd90c968fd9dd7cbb7e72b7f3899ef9429c0
Sam Thurman <sam.bbmgmt@gmail.com>
2020-12-29 17:49:01 -0800

    case when blog init and example queries

---
+++ sql_case_when/examples.sql
@@ -0,0 +1,11 @@
+CASE
+    WHEN condition THEN result
+    ELSE result
+END;
+
+CASE
+    WHEN condition1 THEN result1
+    WHEN condition2 THEN result2
+    WHEN conditionN THEN resultN
+    ELSE result
+END;
